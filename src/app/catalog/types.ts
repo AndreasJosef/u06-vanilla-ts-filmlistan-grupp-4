dd216f99c4a01fa9bb690cbc4014e4932be56f60
Andreas Stratmann <hello.andreas@protonmail.com>
2026-01-21 19:22:22 +0100

    refactor: Simplify CatalogItem interface

--- src/app/catalog/types.ts
+++ src/app/catalog/types.ts
@@ -1,5 +1,5 @@
 // Domain Model
-type CatalogItemBase = {
+export type CatalogItem = {
   tmdb_id: string;
   title: string;
   description: string;
@@ -7,14 +7,6 @@ type CatalogItemBase = {
   release_date: string;
   poster_path: string;
 };
-
-export type CatalogItemDetail = CatalogItemBase & {
-  // TODO refine definition: User different Endpoints like credits, and detail to populate the detail modal with this
-  actors: string; // Credits -> known_for.. acting
-  summary: string; // Some fields from details, tagline and recommendations
-};
-
-export type CatalogItem = CatalogItemBase | CatalogItemDetail;
 
 // View Models
 export type CatalogItemViewModel = CatalogItem & {
