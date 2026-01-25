c4340cc054be6fe049b0ba6bd016c6b2ee0f6020
Andreas Stratmann <hello.andreas@protonmail.com>
2026-01-25 10:44:11 +0100

    refactor: Optimize dbId lookup in detail model

--- src/app/detail/model.ts
+++ src/app/detail/model.ts
@@ -8,23 +8,15 @@ export function getDetailViewModel(state: AppState): DetailViewModel | null {
     return null;
   }
 
-  const watchlistIDs = new Set(state.watchlist.map((movie) => movie.tmdb_id));
-  const isSaved = watchlistIDs.has(focusedMovieId);
-
-  let dbId: string | null = null;
-
-  const savedMovie = state.watchlist.find(
+  const watchlistEntry = state.watchlist.find(
     (item) => item.tmdb_id === focusedMovieId,
   );
 
-  if (savedMovie) {
-    dbId = savedMovie.id;
-  }
-
   return {
     movie: state.movieDetails[focusedMovieId],
-    dbId,
-    isSaved,
+    dbId: watchlistEntry ? watchlistEntry.id : null,
+    // since watchlist entry might be undefined I first force to be a boolean with! but since I want the original truth value and not invert I invert it again !!
+    isSaved: !!watchlistEntry,
     isSeen: false,
   };
 }
